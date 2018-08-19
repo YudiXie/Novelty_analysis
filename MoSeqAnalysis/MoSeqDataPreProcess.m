48c21f98143d730027d4ab42cae94cd80d576506
YuXie <richard9596@gmail.com>
2018-08-19 13:20:55 -0400

    convert data type

--- MoSeqAnalysis/MoSeqDataPreProcess.m
+++ MoSeqAnalysis/MoSeqDataPreProcess.m
@@ -4,5 +4,8 @@ for iter=1:16
     eval(['MSLabels{' num2str(iter) '}=MSlabels' num2str(iter) ';']);
 end
 
+for iter=1:16
+    MSLabels{iter}=double(MSLabels{iter});
+end
 clearvars -except MSLabels MSid
 save('ModelData.mat')
