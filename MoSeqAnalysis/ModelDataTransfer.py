be641769c6bc86e057b0fc32e9c805b1bbd1b59b
YuXie <richard9596@gmail.com>
2018-08-19 12:22:57 -0400

    Add code for data transfer

--- MoSeqAnalysis/ModelDataTransfer.py
+++ MoSeqAnalysis/ModelDataTransfer.py
@@ -1,5 +1,41 @@
-import joblib
-import scipy.io as sio
+# loglikes
+# labels
+# keys
+# heldout_ll
+# model_parameters
+#     kappa
+#     gamma
+#     alpha
+#     nu
+#     num_states
+#     nu_0
+#     sigma_0
+#     kappa_0
+#     nlags
+#     mu_0
+#     model_class
+#     ar_mat
+#     sig
+# run_parameters
+# metadata
+# model
+# hold_out_list
+# train_list
 
-model_data = joblib.load('my_model.p')
-sio.savemat('my_model.mat', {'model_data':model_data})
+import numpy
+import scipy.io as sio
+import joblib
+# import numpy
+# numpy.set_printoptions(threshold=numpy.nan)
+data = joblib.load('my_model.p')
+save_dict={'MSid':data['keys']}
+
+for saveiter in range(16):
+    print(saveiter)
+    MSlabels=data['labels'][0][saveiter][0]
+    print(type(MSlabels))
+    print(len(MSlabels))
+    print(MSlabels)
+    save_dict['MSlabels'+str(saveiter+1)]=MSlabels
+
+sio.savemat('/Users/yuxie/Desktop/my_model.mat', save_dict)
