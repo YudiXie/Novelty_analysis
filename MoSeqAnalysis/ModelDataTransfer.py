73e4299b5a82170868a40bc117fcb2eac13d7a23
Rxie9596 <richard9596@gmail.com>
2018-09-17 17:28:08 -0400

    New version of model data tranfer

--- MoSeqAnalysis/ModelDataTransfer.py
+++ MoSeqAnalysis/ModelDataTransfer.py
@@ -1,45 +1,70 @@
-# MoSeq my_model.p data structure
-#
-# loglikes
-# labels
-# keys
-# heldout_ll
-# model_parameters
-#     kappa
-#     gamma
-#     alpha
-#     nu
-#     num_states
-#     nu_0
-#     sigma_0
-#     kappa_0
-#     nlags
-#     mu_0
-#     model_class
-#     ar_mat
-#     sig
-# run_parameters
-# metadata
-# model
-# hold_out_list
-# train_list
+# load in the model and the index to the extracted data
+index_file = '/media/alex/DataDrive1/MoSeqData/CvsS_180831/CvsS_20180831_MoSeq/moseq2-index.yaml'
+model_file = '/media/alex/DataDrive1/MoSeqData/CvsS_180831/CvsS_20180831_MoSeq/my_model.p'
 
-import numpy
+# Save file directory
+save_directory='/home/alex/Desktop/MoSeqDataFrame.mat'
+
+
 import scipy.io as sio
-import joblib
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from moseq2_viz.model.util import (get_transition_matrix,
+                                   parse_model_results,
+                                   relabel_by_usage)
+from moseq2_viz.util import parse_index
+from moseq2_viz.scalars.util import scalars_to_dataframe
+from moseq2_viz.model.dist import get_behavioral_distance
+
+
+
+
+model_results = parse_model_results(model_file)
+index, sorted_index = parse_index(index_file)
+
+# retrieves behavioral distance using AR matrices
+
+dist = get_behavioral_distance(sorted_index, 
+                               model_file, 
+                               distances=['ar[init]'], 
+                               max_syllable=None)
+
+# packs data into a dataframe, including all scalars and model labels (note that -5 is a fill value, ignore for all downstream analysis)
+
+df = scalars_to_dataframe(sorted_index, include_model=model_file)
+
+sorted_labels = relabel_by_usage(model_results['labels'])[0]
+
+save_dict=df.to_dict("list")
+save_dict['syllable_dis']=dist['ar[init]']
+save_dict['sorted_labels']=sorted_labels
+save_dict['labels']=model_results['labels']
+save_dict['session_uuid']=model_results['keys']
+
+sio.savemat(save_directory, {'MoSeqDataFrame':save_dict})
+
+
+############################################################################
+# Old version
+############################################################################
 # import numpy
-# numpy.set_printoptions(threshold=numpy.nan)
+# import scipy.io as sio
+# import joblib
+# # import numpy
+# # numpy.set_printoptions(threshold=numpy.nan)
 
 
-data = joblib.load('my_model.p')
-save_dict={'MSid':data['keys']}
+# data = joblib.load('my_model.p')
+# save_dict={'MSid':data['keys']}
 
-for saveiter in range(16):
-    print(saveiter)
-    MSlabels=data['labels'][0][saveiter][0]
-    print(type(MSlabels))
-    print(len(MSlabels))
-    print(MSlabels)
-    save_dict['MSlabels'+str(saveiter+1)]=MSlabels
+# for saveiter in range(16):
+#     print(saveiter)
+#     MSlabels=data['labels'][0][saveiter][0]
+#     print(type(MSlabels))
+#     print(len(MSlabels))
+#     print(MSlabels)
+#     save_dict['MSlabels'+str(saveiter+1)]=MSlabels
 
-sio.savemat('/Users/yuxie/Desktop/my_model.mat', save_dict)
+# sio.savemat('/Users/yuxie/Desktop/my_model.mat', save_dict)
