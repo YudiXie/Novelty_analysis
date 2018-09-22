a1f42ab56d677348201f9584f9a9c4bef2dfa309
YuXie <richard9596@gmail.com>
2018-09-22 17:14:27 -0400

    Code for loading photometry data

---
+++ Load_FP_Data.m
@@ -0,0 +1,22 @@
+function [cart_GCaMP, cart_tdTom, cart_water] = load_photometry_raw(fpath)
+disp(['load_photometry_raw: ' fpath]);
+file_ID = fopen(fpath, 'r');
+CC_cart_analog = fread(file_ID, inf, 'double', 0, 'b');
+fclose(file_ID);
+
+if isempty(CC_cart_analog)
+    cart_GCaMP = []; cart_tdTom = []; cart_water = [];
+    return;
+end
+
+A = reshape(CC_cart_analog, 2, [ ]);
+B = reshape (A, 2, 3, [ ]);
+
+cart_GCaMP = B (:,1,:);
+cart_GCaMP = reshape (cart_GCaMP,[],1);
+
+cart_tdTom = B (:,2,:);
+cart_tdTom = reshape (cart_tdTom,[],1);
+
+cart_water = B (:,3,:);
+cart_water = reshape (cart_water,[],1);
