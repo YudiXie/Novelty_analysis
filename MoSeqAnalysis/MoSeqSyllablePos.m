f6736c782c93d591e18f30edb37193255e5df145
YuXie <richard9596@gmail.com>
2019-02-04 08:33:09 -0500

    Plot syllable position of user defined mice and days

--- MoSeqAnalysis/MoSeqSyllablePos.m
+++ MoSeqAnalysis/MoSeqSyllablePos.m
@@ -1,22 +1,49 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% Edit the following parameters
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% !!! Haven't tested yet
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Please edit the following parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 IntSyllable=[63 7 17 52 34];
 Mice_Index_path='/Users/yuxie/Dropbox/YuXie/Kaeser_Lab/190125/Mice_Index.m';
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% Processing
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-load('MoSeqDataFrame.mat')
-Syllablebinedge=[-6,-0.5:1:99.5];
-run(Mice_Index_path);
+Analysis_Mice=1:19;
+Analysis_Days=1;
+
 MarkerSize=5;
 Fsize=20;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Processing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('MoSeqDataFrame.mat');
+cmap=jet(100);
+totalcount=length(MoSeqDataFrame.model_label);
+run(Mice_Index_path);
+
+FilteredLabel=[];
+FilteredXPos=[];
+FilteredYPos=[];
+for miceiter=Analysis_Mice
+
+    for dayiter=Analysis_Days
+
+        for frameiter=1:length(MoSeqDataFrame.SODis)
+            if strcmp(Mice(miceiter).ExpDay(dayiter).MSid, MoSeqDataFrame.uuid(frameiter,:))
+                FilteredLabel=[FilteredLabel double(MoSeqDataFrame.model_label(frameiter))];
+                FilteredXPos=[FilteredXPos MoSeqDataFrame.centroid_x_mm(frameiter)];
+                FilteredYPos=[FilteredYPos MoSeqDataFrame.centroid_y_mm(frameiter)];
+            end
+        end
+
+    end
+
+end
+
+% save('FilteredPos.mat','FilteredLabel','FilteredXPos','FilteredYPos');
+% load('FilteredPos.mat');
 
 SyllableDis=figure;
 
 for syliter=1:length(IntSyllable)
-    XPos=MoSeqDataFrame.centroid_x_mm(MoSeqDataFrame.model_label==IntSyllable(syliter));
-    YPos=MoSeqDataFrame.centroid_y_mm(MoSeqDataFrame.model_label==IntSyllable(syliter));
+    XPos=FilteredXPos(FilteredLabel==IntSyllable(syliter));
+    YPos=FilteredYPos(FilteredLabel==IntSyllable(syliter));
     scatter(XPos,YPos,MarkerSize,'filled')
     hold on
 end
